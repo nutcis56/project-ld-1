ec7a47ce55d012dd8d0be831e18e5aac17d8f9a9
nutcis56 <nutnon009@hotmail.com>
2026-04-30 18:11:37 +0700

    config api

--- api/index.php
+++ api/index.php
@@ -1,6 +1,7 @@
 <?php
 
 $storagePath = '/tmp/storage';
+$bootstrapCachePath = '/tmp/bootstrap/cache';
 
 $directories = [
     $storagePath,
@@ -12,6 +13,8 @@ $directories = [
     $storagePath . '/framework/testing',
     $storagePath . '/framework/views',
     $storagePath . '/logs',
+
+    $bootstrapCachePath,
 ];
 
 foreach ($directories as $directory) {
@@ -20,12 +23,35 @@ foreach ($directories as $directory) {
     }
 }
 
+/**
+ * Laravel storage path
+ */
 putenv('APP_STORAGE_PATH=' . $storagePath);
 $_ENV['APP_STORAGE_PATH'] = $storagePath;
 $_SERVER['APP_STORAGE_PATH'] = $storagePath;
 
+/**
+ * Laravel compiled views path
+ */
 putenv('VIEW_COMPILED_PATH=' . $storagePath . '/framework/views');
 $_ENV['VIEW_COMPILED_PATH'] = $storagePath . '/framework/views';
 $_SERVER['VIEW_COMPILED_PATH'] = $storagePath . '/framework/views';
+
+/**
+ * Laravel bootstrap/cache paths
+ */
+$cacheEnv = [
+    'APP_CONFIG_CACHE' => $bootstrapCachePath . '/config.php',
+    'APP_EVENTS_CACHE' => $bootstrapCachePath . '/events.php',
+    'APP_PACKAGES_CACHE' => $bootstrapCachePath . '/packages.php',
+    'APP_ROUTES_CACHE' => $bootstrapCachePath . '/routes.php',
+    'APP_SERVICES_CACHE' => $bootstrapCachePath . '/services.php',
+];
+
+foreach ($cacheEnv as $key => $value) {
+    putenv($key . '=' . $value);
+    $_ENV[$key] = $value;
+    $_SERVER[$key] = $value;
+}
 
 require __DIR__ . '/../public/index.php';
